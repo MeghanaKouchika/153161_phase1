335128e1310ee8a074dff7e74ff4cd4ed3e7e9b6
MeghanaKouchika <14wh1a0547@bvrithyderabad.edu.in>
2018-07-22 18:57:44 +0530

    Add files via upload

---
+++ Paytm/src/com/capgemini/core/repo/WalletRepo.java
@@ -0,0 +1,19 @@
+package com.capgemini.core.repo;
+
+import java.util.List;
+
+import com.capgemini.core.beans.Customer;
+import com.capgemini.core.beans.Transactions;
+
+public interface WalletRepo {
+
+	public boolean save(Customer customer);
+
+	public Customer findOne(String mobileNo);
+
+	public void remove(String mobileNo);
+	
+	public void addTransaction(String mobileNo, Transactions t);
+	
+	public List<Transactions> getTransaction(String mobileNo);
+}
